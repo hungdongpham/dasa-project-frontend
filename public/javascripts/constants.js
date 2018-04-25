851ab8e31b16f4d24e989da0be126b70913dd96f
Hung Pham <hungdongpham@gmail.com>
2018-04-25 17:24:32 +0700

    Update constant.

--- public/javascripts/constants.js
+++ public/javascripts/constants.js
@@ -1,2 +1,2 @@
-const CLIENT_URL="https://dasa-project-frontend.herokuapp.com";
+const CLIENT_URL="https://dasa-project-frontend.herokuapp.com/";
 const API_URL="https://dasa-project-backend.herokuapp.com/rest/";
