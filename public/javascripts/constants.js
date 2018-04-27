a7174839bbcbb1dced77503b96c9289b641f1d1e
Hung Pham <hungdongpham@gmail.com>
2018-04-27 11:05:54 +0700

    Update constant for deploy heroku.

--- public/javascripts/constants.js
+++ public/javascripts/constants.js
@@ -1,4 +1,4 @@
-const CLIENT_URL="http://localhost:3000/";
-// const CLIENT_URL="https://dasa-project-frontend.herokuapp.com/";
-const API_URL="http://localhost:8080/rest/";
-// const API_URL="https://dasa-project-backend.herokuapp.com/rest/";
+//const CLIENT_URL="http://localhost:3000/";
+const CLIENT_URL="https://dasa-project-frontend.herokuapp.com/";
+//const API_URL="http://localhost:8080/rest/";
+const API_URL="https://dasa-project-backend.herokuapp.com/rest/";
